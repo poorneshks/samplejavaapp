34a76205047575bd6b0c105cccb3afb3717f8d99
poorneshks <132078229+poorneshks@users.noreply.github.com>
2023-05-17 13:47:09 +0530

    Update GenericResourceBundle.java

--- src/main/java/com/devopsdemo/helper/GenericResourceBundle.java
+++ src/main/java/com/devopsdemo/helper/GenericResourceBundle.java
@@ -3,6 +3,8 @@ package com.devopsdemo.helper;
 import java.util.Enumeration;
 import java.util.ResourceBundle;
 
+
+
 public class GenericResourceBundle {
 	public static String getProperties(String source){
 		ResourceBundle rb = ResourceBundle.getBundle("ResourceBundle");
